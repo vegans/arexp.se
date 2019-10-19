0e0e63ef4b820353f170f0147d1a6c14ac74c0e3
Max Malm <benjick@dumfan.net>
2019-10-19 22:21:38 +0200

    Fix compile error

--- pages/find-group/Info.js
+++ pages/find-group/Info.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
-export default ({info}) => (
-  <div>
-    <div style={{maxWidth: 300}}>
-      <h2>{info.city} <span style={{fontSize: '0.6em'}}>({info.country})</span></h2>
-      <p>Reach out here: <a target="_blank" href={info.link}>{info.link}</a></p>
+export default ({info}) => {
+  if (!info) {
+    return null
+  }
+  return (
+    <div>
+      <div style={{maxWidth: 300}}>
+        <h2>{info.city} <span style={{fontSize: '0.6em'}}>({info.country})</span></h2>
+        <p>Reach out here: <a target="_blank" href={info.link}>{info.link}</a></p>
+      </div>
     </div>
-  </div>
-)
+  )
+}
