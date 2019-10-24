23f7d7ca87d46fd3dcc354148682a99fd63514c8
Max Malm <benjick@dumfan.net>
2019-10-24 18:47:13 +0200

    Page: Total Insyn

---
+++ pages/sweden/total-insyn.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Page from '../../layouts/Main'
+import TotalInsyn from '../../content/sweden/TotalInsyn.md'
+
+export default () => (
+  <Page>
+    <TotalInsyn />
+  </Page>
+)
