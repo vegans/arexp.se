389ffc4ffce5cee635a45267e7119b55e5f1e038
Max Malm <benjick@dumfan.net>
2019-10-24 21:13:20 +0200

    Fix background size on iOS

--- components/FullscreenHero.js
+++ components/FullscreenHero.js
@@ -1,33 +1,35 @@
 import React from 'react'
-import Container from 'react-bootstrap/Container'
-import RowUnstyled from 'react-bootstrap/Row'
 import JumbotronUnstyled from 'react-bootstrap/Jumbotron'
 import styled from '@emotion/styled'
 
 const Jumbotron = styled(JumbotronUnstyled)`
-  background-position: 50% 50%;
+  background-position: center center;
   background-attachment: fixed;
   background-repeat: no-repeat;
   background-size: cover;
   height: calc(100vh - 56px);
+  background-image: url(${props => props.img});
+  align-items: end;
+  display: flex;
+  @media (max-width: 768px) {
+    // iOS bug with fixed background
+    background-attachment: scroll;
+  }
 `
 
-const Row = styled(RowUnstyled)`
-  height: 100%;
+const Content = styled.div`
   max-width: 300px;
-  margin: 0 auto;
-  align-items: end;
+  margin-left: auto;
+  margin-right: auto;
   text-align: center;
   color: ${props => (props.light ? '#FCFBE3' : 'rgb(33, 37, 41);')};
 `
 
 const Hero = ({children, image, light}) => (
-  <Jumbotron style={{backgroundImage: `url(${image})`}}>
-    <Container style={{height: '100%'}}>
-      <Row light={light}>
-        <div>{children}</div>
-      </Row>
-    </Container>
+  <Jumbotron img={image}>
+    <Content light={light}>
+      <div>{children}</div>
+    </Content>
   </Jumbotron>
 )
 
