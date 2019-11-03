4c6f2f8c33b6ac45a6a37ad5291bc90afb85271d
Max Malm <benjick@dumfan.net>
2019-11-03 13:11:10 +0100

    Create simple Swedish landing page

--- pages/find-group/index.js
+++ pages/find-group/index.js
@@ -1,7 +1,9 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react'
 import Page from '../../layouts/Main'
 import Text from '../../components/Text'
 import Map from './Map'
+import Link from 'next/link'
 
 const FindGroup = () => {
   return (
@@ -9,10 +11,16 @@ const FindGroup = () => {
       <Map />
       <Text>
         <p>
-          Can&apo;t find a local group? Let&apo;s start one together, reach out
-          to us at{' '}
+          Can't find a local group? Let's start one together, reach out to us at{' '}
           <a href="https://www.facebook.com/AnimalRealityExpose/">Facebook</a>!
         </p>
+        <ul>
+          <li>
+            <Link href="/sweden">
+              <a>ARE Sweden</a>
+            </Link>
+          </li>
+        </ul>
       </Text>
     </Page>
   )
---
+++ pages/sweden/index.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import Link from 'next/link'
+import Page from '../../layouts/Main'
+import Hero from '../../components/FullscreenHero'
+import Text from '../../components/Text'
+
+function Facebook({url, children}) {
+  return (
+    <li>
+      <a
+        href={`http://facebook.com/${url}`}
+        target="_blank"
+        rel="noopener noreferrer">
+        {children}
+      </a>
+    </li>
+  )
+}
+
+export default () => (
+  <Page>
+    <Hero light={true} image="/sweden/gris.jpg">
+      <h2>ARE Sweden</h2>
+    </Hero>
+    <Text>
+      <h3>#totalsinsyn</h3>
+      <p>
+        Total Insyn is a campaign we are currently running in Sweden. We demand
+        total transparancy as a third party into the lives of all animals who
+        are bred to be killed.
+      </p>
+      <p>If you want to run this in your country reach out to us on Faceboo</p>
+      <Link href="/sweden/total-insyn">
+        <a>Read our bill here</a>
+      </Link>
+      <div className="border-top my-3"></div>
+      <h3>Facebook</h3>
+      <ul>
+        <Facebook url="arexpsweden">Animal Reality Exposed</Facebook>
+        <Facebook url="mjolksanningen">Mjölksanningen</Facebook>
+        <Facebook url="svenskgrisproduktion">Svensk grisproduktion</Facebook>
+        <Facebook url="svenskfagelproduktion">Svensk fågelproduktion</Facebook>
+        <Facebook url="gillagrisar">Gilla Grisar</Facebook>
+      </ul>
+    </Text>
+  </Page>
+)
